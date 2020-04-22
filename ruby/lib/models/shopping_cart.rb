358cbeca249568b2319766896b90613b132f0a1c
Chunky <jasonc@nulogy.com>
2020-04-22 12:58:37 -0400

    refactor

--- ruby/lib/models/shopping_cart.rb
+++ ruby/lib/models/shopping_cart.rb
@@ -1,4 +1,6 @@
 class ShoppingCart
+  include SpecialOfferType
+
   attr_reader :items, :product_quantities
 
   def initialize
@@ -31,41 +33,37 @@ class ShoppingCart
 
   def generate_discount(product, quantity, offer, unit_price)
     case offer.offer_type
-    when SpecialOfferType::THREE_FOR_TWO
-      generate_x_for_y_discount(product, quantity, offer, unit_price, 3, 2)
-    when SpecialOfferType::TWO_FOR_AMOUNT
-      x = 2
+    when THREE_FOR_TWO
+      generate_x_for_y_discount(product, quantity, unit_price, 3, 2)
+    when TWO_FOR_AMOUNT
       amount = offer.argument
-      if quantity >= x
-        quantity_as_int = quantity.to_i
-        number_of_x = quantity_as_int / x
-        total = amount * number_of_x + quantity_as_int % x * unit_price
-        discount_amount = unit_price * quantity - total
-        Discount.new(product, "#{x} for #{amount}", discount_amount)
-      end
-    when SpecialOfferType::FIVE_FOR_AMOUNT
-      x = 5
+      generate_x_for_amount_discount(product, quantity, unit_price, 2, amount)
+    when FIVE_FOR_AMOUNT
       amount = offer.argument
-      if quantity >= x
-        quantity_as_int = quantity.to_i
-        number_of_x = quantity_as_int / x
-        total = amount * number_of_x + quantity_as_int % x * unit_price
-        discount_amount = unit_price * quantity - total
-        Discount.new(product, "#{x} for #{amount}", discount_amount)
-      end
-    when SpecialOfferType::TEN_PERCENT_DISCOUNT
+      generate_x_for_amount_discount(product, quantity, unit_price, 5, amount)
+    when TEN_PERCENT_DISCOUNT
       generate_percentage_discount(product, quantity, offer, unit_price)
     end
   end
 
-  def generate_x_for_y_discount(product, quantity, _offer, unit_price, x, y)
-    if quantity >= x
-      quantity_as_int = quantity.to_i
-      number_of_x = quantity_as_int / x
-      total = (number_of_x * y * unit_price) + quantity_as_int % x * unit_price
-      discount_amount = quantity * unit_price - total
-      Discount.new(product, "#{x} for #{y}", discount_amount)
-    end
+  def generate_x_for_y_discount(product, quantity, unit_price, x, y)
+    return if quantity < x
+
+    quantity_as_int = quantity.to_i
+    number_of_x = quantity_as_int / x
+    total = (number_of_x * y * unit_price) + quantity_as_int % x * unit_price
+    discount_amount = quantity * unit_price - total
+    Discount.new(product, "#{x} for #{y}", discount_amount)
+  end
+
+  def generate_x_for_amount_discount(product, quantity, unit_price, x, amount)
+    return if quantity < x
+
+    quantity_as_int = quantity.to_i
+    number_of_x = quantity_as_int / x
+    total = amount * number_of_x + quantity_as_int % x * unit_price
+    discount_amount = unit_price * quantity - total
+    Discount.new(product, "#{x} for #{amount}", discount_amount)
   end
 
   def generate_percentage_discount(product, quantity, offer, unit_price)
