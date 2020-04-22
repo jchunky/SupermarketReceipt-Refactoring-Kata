4788912aacbe636078473b83008f4f2534a1df41
Chunky <jasonc@nulogy.com>
2020-04-21 23:04:06 -0400

    refactor

--- ruby/lib/receipt_printer.rb
+++ ruby/lib/receipt_printer.rb
@@ -7,47 +7,46 @@ class ReceiptPrinter
     result = ''
     receipt.items.each do |item|
       price = '%.2f' % item.total_price
-      quantity = self.class.present_quantity(item)
+      quantity = present_quantity(item)
       name = item.product.name
       unit_price = '%.2f' % item.price
 
       whitespace_size = @columns - name.size - price.size
-      line = name + self.class.whitespace(whitespace_size) + price + "\n"
+      line = name + whitespace(whitespace_size) + price + "\n"
 
       line += '  ' + unit_price + ' * ' + quantity + "\n" if item.quantity != 1
 
-      result.concat(line)
+      result << line
     end
     receipt.discounts.each do |discount|
       product_presentation = discount.product.name
       price_presentation = '%.2f' % discount.discount_amount
       description = discount.description
-      result.concat(description)
-      result.concat('(')
-      result.concat(product_presentation)
-      result.concat(')')
-      result.concat(self.class.whitespace(@columns - 3 - product_presentation.size - description.size - price_presentation.size))
-      result.concat('-')
-      result.concat(price_presentation)
-      result.concat("\n")
+
+      result << description
+      result << '('
+      result << product_presentation
+      result << ')'
+      result << whitespace(@columns - 3 - product_presentation.size - description.size - price_presentation.size)
+      result << '-'
+      result << price_presentation
+      result << "\n"
     end
-    result.concat("\n")
+    result << "\n"
     price_presentation = '%.2f' % receipt.total_price.to_f
     total = 'Total: '
-    whitespace = self.class.whitespace(@columns - total.size - price_presentation.size)
-    result.concat(total, whitespace, price_presentation)
+    whitespace = whitespace(@columns - total.size - price_presentation.size)
+    result << total << whitespace << price_presentation
     result.to_s
   end
 
-  def self.present_quantity(item)
+  private
+
+  def present_quantity(item)
     ProductUnit::EACH == item.product.unit ? '%x' % item.quantity.to_i : '%.3f' % item.quantity
   end
 
-  def self.whitespace(whitespace_size)
-    whitespace = ''
-    whitespace_size.times do
-      whitespace.concat(' ')
-    end
-    whitespace
+  def whitespace(length)
+    ' ' * length
   end
 end
