e4cdb7de0228ee59b2a81bc615765bb55f4ec213
Chunky <jasonc@nulogy.com>
2020-04-22 09:08:38 -0400

    Add color to minitest

--- ruby/test/test_helper.rb
+++ ruby/test/test_helper.rb
@@ -1,4 +1,7 @@
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true)]
 
 pattern = File.join(File.dirname(__FILE__), '..', 'lib', '**', '*.rb')
 Dir[pattern].each { |filepath| require_relative filepath }
