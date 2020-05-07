7e689c14058d4abbac66968db1b9b48e0fa99c43
Chunky <jasonc@nulogy.com>
2020-05-07 00:38:35 -0400

    refactor

--- ruby/lib/receipt_printer.rb
+++ ruby/lib/receipt_printer.rb
@@ -4,28 +4,35 @@ class ReceiptPrinter
   end
 
   def print_receipt(receipt)
-    result = []
-    receipt.items.each do |item|
-      price = format_price(item.total_price)
-      quantity = format_quantity(item)
-      product = item.product.name
-      unit_price = format_price(item.price)
-      result << format_line(product, price)
-      result << "  #{unit_price} * #{quantity}" if item.quantity != 1
-    end
-    receipt.discounts.each do |discount|
-      product = discount.product.name
-      discount_amount = format_price(discount.discount_amount)
-      description = discount.description
-      result << format_line("#{description}(#{product})", "-#{discount_amount}")
-    end
     total_price = format_price(receipt.total_price)
-    result << ""
-    result << format_line("Total:", total_price)
-    result.join("\n")
+
+    [
+      receipt.items.map(&method(:item_lines)),
+      receipt.discounts.map(&method(:discount_lines)),
+      "",
+      format_line("Total:", total_price),
+    ].join("\n")
   end
 
   private
+
+  def item_lines(item)
+    price = format_price(item.total_price)
+    quantity = format_quantity(item)
+    product = item.product.name
+    unit_price = format_price(item.price)
+    [
+      format_line(product, price),
+      ("  #{unit_price} * #{quantity}" if item.quantity != 1),
+    ].compact
+  end
+
+  def discount_lines(discount)
+    product = discount.product.name
+    discount_amount = format_price(discount.discount_amount)
+    description = discount.description
+    format_line("#{description}(#{product})", "-#{discount_amount}")
+  end
 
   def format_price(price)
     format("%.2f", price)
@@ -36,6 +43,10 @@ class ReceiptPrinter
   end
 
   def format_quantity(item)
-    ProductUnit::EACH == item.product.unit ? "%x" % item.quantity.to_i : "%.3f" % item.quantity
+    case item.product.unit
+    when ProductUnit::EACH then format("%x", item.quantity)
+    when ProductUnit::KILO then format("%.3f", item.quantity)
+    else raise "Unexpected ProductUnit: #{item.product.unit}"
+    end
   end
 end
