0a2572f48266c2a39af8005adf803fefb843a67c
Chunky <jasonc@nulogy.com>
2020-05-07 00:02:59 -0400

    refactor

--- ruby/lib/models/discount.rb
+++ ruby/lib/models/discount.rb
@@ -1,9 +1,1 @@
-class Discount
-  attr_reader :product, :description, :discount_amount
-
-  def initialize(product, description, discount_amount)
-    @product = product
-    @description = description
-    @discount_amount = discount_amount
-  end
-end
+Discount = Struct.new(:product, :description, :discount_amount)
--- ruby/lib/models/offer.rb
+++ ruby/lib/models/offer.rb
@@ -1,9 +1,1 @@
-class Offer
-  attr_reader :product, :offer_type, :argument
-
-  def initialize(offer_type, product, argument)
-    @offer_type = offer_type
-    @argument = argument
-    @product = product
-  end
-end
+Offer = Struct.new(:offer_type, :product, :argument)
