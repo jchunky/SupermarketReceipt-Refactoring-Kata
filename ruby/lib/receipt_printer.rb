e760956b9ab7ce1256555d4df139fc71d75f6b30
Chunky <jasonc@nulogy.com>
2020-05-28 09:54:27 -0400

    refactor

--- ruby/lib/receipt_printer.rb
+++ ruby/lib/receipt_printer.rb
@@ -6,13 +6,13 @@ class ReceiptPrinter
   def print_receipt(receipt)
     result = ""
     receipt.items.each do |item|
-      price = "%.2f" % item.total_price
-      quantity = self.class.present_quantity(item)
+      price = format_price(item.total_price)
+      quantity = format_quantity(item)
       name = item.product.name
-      unit_price = "%.2f" % item.price
+      unit_price = format_price(item.price)
 
       whitespace_size = @columns - name.size - price.size
-      line = name + self.class.whitespace(whitespace_size) + price + "\n"
+      line = name + whitespace(whitespace_size) + price + "\n"
 
       line += "  " + unit_price + " * " + quantity + "\n" if item.quantity != 1
 
@@ -20,34 +20,40 @@ class ReceiptPrinter
     end
     receipt.discounts.each do |discount|
       product_presentation = discount.product.name
-      price_presentation = "%.2f" % discount.discount_amount
+      price_presentation = format_price(discount.discount_amount)
       description = discount.description
       result.concat(description)
       result.concat("(")
       result.concat(product_presentation)
       result.concat(")")
-      result.concat(self.class.whitespace(@columns - 3 - product_presentation.size - description.size - price_presentation.size))
+      result.concat(whitespace(@columns - 3 - product_presentation.size - description.size - price_presentation.size))
       result.concat("-")
       result.concat(price_presentation)
       result.concat("\n")
     end
     result.concat("\n")
-    price_presentation = "%.2f" % receipt.total_price.to_f
+    price_presentation = format_price(receipt.total_price)
     total = "Total: "
-    whitespace = self.class.whitespace(@columns - total.size - price_presentation.size)
+    whitespace = whitespace(@columns - total.size - price_presentation.size)
     result.concat(total, whitespace, price_presentation)
-    result.to_s
+    result
   end
 
-  def self.present_quantity(item)
-    ProductUnit::EACH == item.product.unit ? "%x" % item.quantity.to_i : "%.3f" % item.quantity
+  private
+
+  def format_price(price)
+    "%.2f" % price
   end
 
-  def self.whitespace(whitespace_size)
-    whitespace = ""
-    whitespace_size.times do
-      whitespace.concat(" ")
+  def format_quantity(item)
+    if ProductUnit::EACH == item.product.unit
+      "%x" % item.quantity
+    else
+      "%.3f" % item.quantity
     end
-    whitespace
+  end
+
+  def whitespace(size)
+    " " * size
   end
 end
