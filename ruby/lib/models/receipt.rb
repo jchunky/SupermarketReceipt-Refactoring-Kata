287b779327eb3116db73de481bde3fc727e72f5c
Chunky <jasonc@nulogy.com>
2020-05-30 11:20:16 -0400

    refactor

--- ruby/lib/models/receipt.rb
+++ ruby/lib/models/receipt.rb
@@ -1,20 +1,5 @@
-class Receipt
-  attr_reader :items, :discounts
-
-  def initialize
-    @items = []
-    @discounts = []
-  end
-
+Receipt = Struct.new(:items, :discounts) do
   def total_price
     items.sum(&:total_price) - discounts.sum(&:discount_amount)
-  end
-
-  def add_receipt_item(receipt_item)
-    items << receipt_item
-  end
-
-  def add_discount(discount)
-    discounts << discount
   end
 end
--- ruby/lib/models/teller.rb
+++ ruby/lib/models/teller.rb
@@ -1,48 +1,41 @@
 class Teller
+  attr_reader :offers, :items
+
   def initialize
     @offers = {}
     @items = []
   end
 
   def add_item_quantity(product, quantity)
-    @items << ReceiptItem.new(product, quantity)
+    items << ReceiptItem.new(product, quantity)
   end
 
   def add_special_offer(offer_type, product, argument)
-    @offers[product] = Offer.new(offer_type, product, argument)
+    offers[product] = Offer.new(offer_type, product, argument)
   end
 
   def receipt
-    receipt = Receipt.new
-
-    @items.each { |item| receipt.add_receipt_item(item) }
-
-    product_quantities.each do |product, quantity|
-      discount = find_discount(product, quantity)
-      next unless discount
-
-      receipt.add_discount(discount)
-    end
-
-    receipt
+    Receipt.new(items, discounts)
   end
 
   private
 
+  def discounts
+    items
+      .group_by(&:product)
+      .transform_values { |items| items.sum(&:quantity) }
+      .map(&method(:find_discount))
+      .compact
+  end
+
   def find_discount(product, quantity)
     description, discount_amount = DiscountCalculator.get_discount(
-      @offers[product],
+      offers[product],
       quantity,
       product.unit_price
     )
     return unless discount_amount
 
     Discount.new(product, description, discount_amount)
-  end
-
-  def product_quantities
-    @items.group_by(&:product).transform_values do |items|
-      items.sum(&:quantity)
-    end
   end
 end
