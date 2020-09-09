6e270d8ca5a9e9a4e4ecb3701138fbf1c820344c
Chunky <jasonc@nulogy.com>
2020-09-09 17:22:13 -0400

    refactor

--- ruby/lib/models/shopping_cart.rb
+++ ruby/lib/models/shopping_cart.rb
@@ -23,27 +23,25 @@ class ShoppingCart
   def calculate_discount(offer, unit_price, p, quantity)
     case offer.offer_type
     when SpecialOfferType::PERCENT_DISCOUNT
-      percent_discount(offer, unit_price, p, quantity)
+      percent_discount(unit_price, p, quantity, offer.argument)
     when SpecialOfferType::THREE_FOR_TWO
-      buy_2_get_1_free(offer, unit_price, p, quantity)
+      buy_2_get_1_free(unit_price, p, quantity)
     when SpecialOfferType::TWO_FOR_AMOUNT
-      buy_x_quantity_for_fixed_price(offer, unit_price, p, quantity, 2)
+      buy_x_quantity_for_fixed_price(unit_price, p, quantity, offer.argument, 2)
     when SpecialOfferType::FIVE_FOR_AMOUNT
-      buy_x_quantity_for_fixed_price(offer, unit_price, p, quantity, 5)
+      buy_x_quantity_for_fixed_price(unit_price, p, quantity, offer.argument, 5)
     else
       raise "Unexpected offer type: #{offer.offer_type}"
     end
   end
 
-  def percent_discount(offer, unit_price, p, quantity)
-    discount_percentage = offer.argument
-
-    discount_amount = quantity * unit_price * offer.argument / 100.0
+  def percent_discount(unit_price, p, quantity, discount_percentage)
+    discount_amount = quantity * unit_price * discount_percentage / 100.0
 
     Discount.new(p, "#{discount_percentage}% off", discount_amount)
   end
 
-  def buy_2_get_1_free(_offer, unit_price, p, quantity)
+  def buy_2_get_1_free(unit_price, p, quantity)
     discount_divisor = 3
     return nil if quantity < discount_divisor
 
@@ -62,10 +60,8 @@ class ShoppingCart
     Discount.new(p, "#{discount_divisor} for 2", discount_amount)
   end
 
-  def buy_x_quantity_for_fixed_price(offer, unit_price, p, quantity, discount_divisor)
+  def buy_x_quantity_for_fixed_price(unit_price, p, quantity, discount_price, discount_divisor)
     return nil if quantity < discount_divisor
-
-    discount_price = offer.argument
 
     number_of_discounts = quantity.to_i / discount_divisor
     number_of_fullprice_items = quantity.to_i % discount_divisor
