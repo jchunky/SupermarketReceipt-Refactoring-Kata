cf62ce7ca8ffe915d6b32b0c4ccc12996d76b937
Chunky <jasonc@nulogy.com>
2020-12-15 22:01:50 -0500

    refactor

--- ruby/lib/models/teller.rb
+++ ruby/lib/models/teller.rb
@@ -1,27 +1,30 @@
 class Teller
+  attr_reader :catalog, :offers
+
   def initialize(catalog)
     @catalog = catalog
     @offers = {}
   end
 
   def add_special_offer(offer_type, product, argument)
-    @offers[product] = Offer.new(offer_type, product, argument)
+    offers[product] = Offer.new(offer_type, product, argument)
   end
 
-  def checks_out_articles_from(the_cart)
+  def checks_out_articles_from(cart)
     receipt = Receipt.new
-    product_quantities = the_cart.items
-    product_quantities.each do |pq|
-      p = pq.product
-      quantity = pq.quantity
-      unit_price = @catalog.unit_price(p)
-      price = quantity * unit_price
-      ri = ReceiptItem.new(p, quantity, unit_price, price)
-      # receipt.add_product(ri)
-      receipt.add_product(p, quantity, unit_price, price)
+    cart.items.each do |product_quantity|
+      receipt_item = build_receipt_item(product_quantity)
+      receipt.add_receipt_item(receipt_item)
     end
-    the_cart.handle_offers(receipt, @offers, @catalog)
-
+    cart.handle_offers(receipt, offers, catalog)
     receipt
+  end
+
+  def build_receipt_item(product_quantity)
+    product = product_quantity.product
+    quantity = product_quantity.quantity
+    unit_price = catalog.unit_price(product)
+    total_price = quantity * unit_price
+    ReceiptItem.new(product, quantity, unit_price, total_price)
   end
 end
