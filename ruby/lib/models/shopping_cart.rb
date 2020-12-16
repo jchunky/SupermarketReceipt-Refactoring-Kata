13996600e4b0ad1078bb4d49ce34b9d5c59731af
Chunky <jasonc@nulogy.com>
2020-12-15 22:14:12 -0500

    refactor

--- ruby/lib/models/shopping_cart.rb
+++ ruby/lib/models/shopping_cart.rb
@@ -1,9 +1,8 @@
 class ShoppingCart
-  attr_reader :items, :product_quantities
+  attr_reader :items
 
   def initialize
     @items = []
-    @product_quantities = {}
   end
 
   def add_item(product)
@@ -12,60 +11,67 @@ class ShoppingCart
 
   def add_item_quantity(product, quantity)
     items << ProductQuantity.new(product, quantity)
-    product_quantities[product] ||= 0
-    product_quantities[product] += quantity
+  end
+
+  def product_quantities
+    @product_quantities ||= items.group_by(&:product).transform_values { |pq| pq.sum(&:quantity) }
   end
 
   def handle_offers(receipt, offers, catalog)
     product_quantities.each_key do |p|
-      quantity = @product_quantities[p]
       next unless offers.key?(p)
 
-      offer = offers[p]
-      unit_price = catalog.unit_price(p)
-      quantity_as_int = quantity.to_i
-      discount = nil
-      x = 1
-      case offer.offer_type
-      when SpecialOfferType::THREE_FOR_TWO
-        x = 3
-
-      when SpecialOfferType::TWO_FOR_AMOUNT
-        x = 2
-        if quantity_as_int >= 2
-          total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
-          discount_n = unit_price * quantity - total
-          discount = Discount.new(
-            p,
-            "2 for #{offer.argument}",
-            discount_n
-          )
-        end
-
-      end
-      x = 5 if offer.offer_type == SpecialOfferType::FIVE_FOR_AMOUNT
-      number_of_x = quantity_as_int / x
-      if offer.offer_type == SpecialOfferType::THREE_FOR_TWO && quantity_as_int > 2
-        discount_amount = quantity * unit_price - ((number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-        discount = Discount.new(p, "3 for 2", discount_amount)
-      end
-      if offer.offer_type == SpecialOfferType::TEN_PERCENT_DISCOUNT
-        discount = Discount.new(
-          p,
-          "#{offer.argument}% off",
-          quantity * unit_price * offer.argument / 100.0
-        )
-      end
-      if offer.offer_type == SpecialOfferType::FIVE_FOR_AMOUNT && quantity_as_int >= 5
-        discount_total = unit_price * quantity - (offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
-        discount = Discount.new(
-          p,
-          "#{x} for #{offer.argument}",
-          discount_total
-        )
-      end
+      discount = calculate_discount(offers, catalog, p)
 
       receipt.add_discount(discount) if discount
     end
+  end
+
+  def calculate_discount(offers, catalog, p)
+    quantity = product_quantities[p]
+    offer = offers[p]
+    unit_price = catalog.unit_price(p)
+    quantity_as_int = quantity.to_i
+    discount = nil
+    x = 1
+    case offer.offer_type
+    when SpecialOfferType::THREE_FOR_TWO
+      x = 3
+
+    when SpecialOfferType::TWO_FOR_AMOUNT
+      x = 2
+      if quantity_as_int >= 2
+        total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
+        discount_n = unit_price * quantity - total
+        discount = Discount.new(
+          p,
+          "2 for #{offer.argument}",
+          discount_n
+        )
+      end
+
+    end
+    x = 5 if offer.offer_type == SpecialOfferType::FIVE_FOR_AMOUNT
+    number_of_x = quantity_as_int / x
+    if offer.offer_type == SpecialOfferType::THREE_FOR_TWO && quantity_as_int > 2
+      discount_amount = quantity * unit_price - ((number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+      discount = Discount.new(p, "3 for 2", discount_amount)
+    end
+    if offer.offer_type == SpecialOfferType::TEN_PERCENT_DISCOUNT
+      discount = Discount.new(
+        p,
+        "#{offer.argument}% off",
+        quantity * unit_price * offer.argument / 100.0
+      )
+    end
+    if offer.offer_type == SpecialOfferType::FIVE_FOR_AMOUNT && quantity_as_int >= 5
+      discount_total = unit_price * quantity - (offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
+      discount = Discount.new(
+        p,
+        "#{x} for #{offer.argument}",
+        discount_total
+      )
+    end
+    discount
   end
 end
