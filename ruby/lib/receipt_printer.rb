4ac136e3b3cd9258d393c7beb50a0c10674f9bd1
Chunky <jasonc@nulogy.com>
2020-12-22 09:40:53 -0500

    refactor

--- ruby/lib/receipt_printer.rb
+++ ruby/lib/receipt_printer.rb
@@ -6,52 +6,43 @@ class ReceiptPrinter
   end
 
   def print_receipt(receipt)
-    result = []
-    receipt.items.each do |item|
-      result << format_item(item)
-    end
-    receipt.discounts.each do |discount|
-      result << format_discount(discount)
-      result << "\n"
-    end
-    result << "\n"
-    result << format_line("Total: ", format_price(receipt.total_price))
-    result.join
+    [
+      receipt.items.map(&method(:format_item)),
+      receipt.discounts.map(&method(:format_discount)),
+      "",
+      format_line("Total: ", format_price(receipt.total_price)),
+    ].flatten.compact.join("\n")
   end
 
   private
 
   def format_item(item)
-    price = format_price(item.total_price)
-    quantity = format_quantity(item)
-    name = item.product.name
-    unit_price = format_price(item.price)
-    line = format_line(name, price)
-    line += "\n"
-    line += "  #{unit_price} * #{quantity}\n" if item.quantity != 1
-    line
+    [
+      format_line(item.product.name, format_price(item.total_price)),
+      (format("  %s * %s", format_price(item.price), format_quantity(item)) if item.quantity != 1),
+    ]
   end
 
-  def format_price(price)
-    "%.2f" % price
+  def format_discount(discount)
+    discount_amount = format_price(discount.discount_amount)
+    description = format("%s(%s)", discount.description, discount.product.name)
+    format_line(description, "-#{discount_amount}")
   end
 
   def format_line(left, right)
     [left, right.rjust(columns - left.size)].join
   end
 
+  def format_price(price)
+    format("%.2f", price)
+  end
+
   def format_quantity(item)
     case item.product.unit
     when ProductUnit::EACH
-      "%x" % item.quantity.to_i
+      format("%x", item.quantity.to_i)
     when ProductUnit::KILO
-      "%.3f" % item.quantity
+      format("%.3f", item.quantity)
     end
-  end
-
-  def format_discount(discount)
-    price_presentation = format_price(discount.discount_amount)
-    discount_description = "#{discount.description}(#{discount.product.name})"
-    format_line(discount_description, "-#{price_presentation}")
   end
 end
