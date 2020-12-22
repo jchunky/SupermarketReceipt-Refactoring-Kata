8e8dcb854040ca7c37d8203f4ee028798841b092
Chunky <jasonc@nulogy.com>
2020-12-22 10:16:41 -0500

    refactor

--- ruby/lib/models/shopping_cart.rb
+++ ruby/lib/models/shopping_cart.rb
@@ -27,51 +27,44 @@ class ShoppingCart
   private
 
   def build_discount(catalog, offers, product_quantity)
-    quantity = product_quantities[product_quantity]
-
     offer = offers[product_quantity]
+    quantity = product_quantities[product_quantity]
     unit_price = catalog.unit_price(product_quantity)
     quantity_as_int = quantity.to_i
-    discount = nil
-    x = 1
+
     case offer.offer_type
     when SpecialOfferType::THREE_FOR_TWO
       x = 3
+      y = 2
+      number_of_x = quantity_as_int / x
+      return unless quantity_as_int >= x
 
+      total = number_of_x * y * unit_price + quantity_as_int % x * unit_price
+      discount_amount = quantity * unit_price - total
+      Discount.new(product_quantity, "#{x} for #{y}", discount_amount)
+    when SpecialOfferType::TEN_PERCENT_DISCOUNT
+      percentage_discount = offer.argument
+      total = percentage_discount / 100.0
+      discount_total = quantity * unit_price * total
+      Discount.new(product_quantity, "#{percentage_discount}% off", discount_total)
     when SpecialOfferType::TWO_FOR_AMOUNT
       x = 2
-      if quantity_as_int >= 2
-        total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
-        discount_n = unit_price * quantity - total
-        discount = Discount.new(
-          product_quantity,
-          "2 for #{offer.argument}",
-          discount_n
-        )
-      end
+      number_of_x = quantity_as_int / x
+      group_amount = offer.argument
+      return unless quantity_as_int >= x
 
+      total = group_amount * number_of_x + quantity_as_int % x * unit_price
+      discount_total = unit_price * quantity - total
+      Discount.new(product_quantity, "#{x} for #{group_amount}", discount_total)
+    when SpecialOfferType::FIVE_FOR_AMOUNT
+      x = 5
+      number_of_x = quantity_as_int / x
+      group_amount = offer.argument
+      return unless quantity_as_int >= x
+
+      total = group_amount * number_of_x + quantity_as_int % x * unit_price
+      discount_total = unit_price * quantity - total
+      Discount.new(product_quantity, "#{x} for #{group_amount}", discount_total)
     end
-    x = 5 if offer.offer_type == SpecialOfferType::FIVE_FOR_AMOUNT
-    number_of_x = quantity_as_int / x
-    if offer.offer_type == SpecialOfferType::THREE_FOR_TWO && quantity_as_int > 2
-      discount_amount = quantity * unit_price - ((number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-      discount = Discount.new(product_quantity, "3 for 2", discount_amount)
-    end
-    if offer.offer_type == SpecialOfferType::TEN_PERCENT_DISCOUNT
-      discount = Discount.new(
-        product_quantity,
-        "#{offer.argument}% off",
-        quantity * unit_price * offer.argument / 100.0
-      )
-    end
-    if offer.offer_type == SpecialOfferType::FIVE_FOR_AMOUNT && quantity_as_int >= 5
-      discount_total = unit_price * quantity - (offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
-      discount = Discount.new(
-        product_quantity,
-        "#{x} for #{offer.argument}",
-        discount_total
-      )
-    end
-    discount
   end
 end
