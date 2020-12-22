d1c379fb07ccb77d3957ba0ca3059adc805e0e4c
Chunky <jasonc@nulogy.com>
2020-12-22 09:46:48 -0500

    refactor

--- ruby/lib/models/receipt.rb
+++ ruby/lib/models/receipt.rb
@@ -1,35 +1,20 @@
 class Receipt
+  attr_reader :items, :discounts
+
   def initialize
     @items = []
     @discounts = []
   end
 
   def total_price
-    total = 0.0
-    @items.each do |item|
-      total += item.total_price
-    end
-    @discounts.each do |discount|
-      total -= discount.discount_amount
-    end
-    total
+    items.sum(&:total_price) - discounts.sum(&:discount_amount)
   end
 
-  def add_product(product, quantity, price, total_price)
-    @items << ReceiptItem.new(product, quantity, price, total_price)
-    nil
-  end
-
-  def items
-    Array.new @items
+  def add_item(item)
+    items << item
   end
 
   def add_discount(discount)
-    @discounts << discount
-    nil
-  end
-
-  def discounts
-    Array.new @discounts
+    discounts << discount
   end
 end
