73c27fafe33dc91b760dbaaa85a96e9cc0ed0d37
Chunky <jasonc@nulogy.com>
2020-12-22 09:53:17 -0500

    refactor

--- ruby/lib/models/teller.rb
+++ ruby/lib/models/teller.rb
@@ -1,26 +1,34 @@
 class Teller
+  attr_reader :offers, :catalog
+
   def initialize(catalog)
     @catalog = catalog
     @offers = {}
   end
 
   def add_special_offer(offer_type, product, argument)
-    @offers[product] = Offer.new(offer_type, product, argument)
+    offers[product] = Offer.new(offer_type, product, argument)
   end
 
-  def checks_out_articles_from(the_cart)
+  def checks_out_articles_from(cart)
     receipt = Receipt.new
-    product_quantities = the_cart.items
-    product_quantities.each do |pq|
-      p = pq.product
-      quantity = pq.quantity
-      unit_price = @catalog.unit_price(p)
-      price = quantity * unit_price
-      item = ReceiptItem.new(p, quantity, unit_price, price)
-      receipt.add_item(item)
-    end
-    the_cart.handle_offers(receipt, @offers, @catalog)
+
+    cart.items
+      .map { |pq| build_receipt_item(pq) }
+      .each { |item| receipt.add_item(item) }
+
+    cart.handle_offers(receipt, offers, catalog)
 
     receipt
+  end
+
+  private
+
+  def build_receipt_item(product_quantity)
+    product = product_quantity.product
+    quantity = product_quantity.quantity
+    unit_price = catalog.unit_price(product)
+    total_price = quantity * unit_price
+    ReceiptItem.new(product, quantity, unit_price, total_price)
   end
 end
